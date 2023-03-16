dd00c53021285b1d666c077127ae35729931e5b7
unknown <paortegon@uniboyaca.edu.co>
2023-03-16 16:58:59 -0500

    Actividad realizada

--- jsPrueba/saludar.js
+++ jsPrueba/saludar.js
@@ -1,14 +1,24 @@
 // document.getElementById('nombre').setAttribute('value', 'johan');
 
+// document.getElementById('saludarBtn')
+//     .addEventListener('click', function(){
+//         let nombrePersona= document.getElementById('nombre').value;
+//         // if(nombrePersona=='' || nombrePersona==null){
+//         if(!nombrePersona){
+//         alert('Debe ingresar el nombre')
+//         }else{
+//         alert('hola ' + nombrePersona + '!!!')
+//         }
+//     });
+
 document.getElementById('saludarBtn')
-    .addEventListener('click', function(){
+    .addEventListener('click', function (){
         let nombrePersona= document.getElementById('nombre').value;
-        // if(nombrePersona=='' || nombrePersona==null){
-        if(!nombrePersona){
-            alert('Debe ingresar el nombre')
-        }else{
-        alert('hola ' + nombrePersona + '!!!')
-        }
+         if(!nombrePersona){
+         document.getElementById("cambio").innerHTML=("Debe ingresar el nombre<h1>");
+         }else{
+         document.getElementById("cambio").innerHTML=("hola "+ nombrePersona);
+         }
     });
 
     
